fd777ff3a19665f205619d70113adce6e97febf0
David Cooley <david@leonardo.local>
2016-11-08 18:02:15 +1100

    min.js is not 'binding'

--- inst/htmlwidgets/vizicitiesway.js
+++ inst/htmlwidgets/vizicitiesway.js
@@ -13,30 +13,24 @@ HTMLWidgets.widget({
       renderValue: function(x) {
 
         // TODO: code to render the widget, e.g.
-        //el.innerText = x.message;
+       el.innerText = x.message;
 
-        // Manhattan
-        console.log(el.id);
-        var vizicities = document.getElementById(el.id);
-        vizicities.className = "vizicities";
-        console.log(vizicities);
+       // London
+			var coords = [51.5052, -0.0308];
+
+			var world = VIZI.world('world', {
+			  skybox: false,
+			  postProcessing: false
+			}).setView(coords);
 
 
-				var coords = [40.739940, -73.988801];
-				//var world = VIZI.world('vizicities').setView(coords);
-				var world = VIZI.world(vizicities).setView(coords);
-				VIZI.Controls.orbit().addTo(world);
 
-				VIZI.imageTileLayer('http://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
-}).addTo(world);
 
       },
 
       resize: function(width, height) {
 
         // TODO: code to re-render the widget with a new size
-
       }
 
     };
